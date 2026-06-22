cdf8f7aa8fe4ff7bd1649e68447f220123d16772
yuanzz <1149772382@qq.com>
2026-06-22 09:47:03 +0800

    chore: add GitHub Pages deployment

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,8 @@ import { resolve } from 'node:path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 
+const githubRepoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'Perler_Beads_Studio'
+
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -10,6 +12,7 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    base: process.env.GITHUB_PAGES === 'true' ? `/${githubRepoName}/` : './',
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
